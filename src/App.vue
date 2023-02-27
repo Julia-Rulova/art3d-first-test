96ea33595d1c5bb6175a6e5429a7d6705a2256f8
Julia-Rulova <julia.rulova0066@gmail.com>
2023-02-27 16:56:23 +0300

    all

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,52 @@
       </div>
 
       <form class="app__form">
-        <input-component :label="'NAME'" :id="'name'" :type="'string'" />
-        <input-component :label="'PHONE'" :id="'phone'" :type="'number'" />
-        <input-component :label="'TEMP'" :id="'temp'" :type="'range'" />
+        <input-component
+          :label="'NAME'"
+          :id="'name'"
+          :type="'text'"
+          :require="true"
+          :error="errors.name"
+          :modelValue="form.nameValue"
+          @update="updateName"
+        />
+        <input-component
+          :label="'PHONE'"
+          :id="'phone'"
+          :type="'text'"
+          :require="true"
+          :error="errors.phone"
+          :modelValue="form.phoneValue"
+          @update="updatePhone"
+        />
+        <input-component
+          :label="'TEMP'"
+          :id="'temp'"
+          :type="'range'"
+          :modelValue="form.tempValue"
+          @update="updateTemp"
+        />
         <input-component
           :label="'COMMENTS'"
           :id="'comments'"
-          :type="'string'"
+          :type="'text'"
+          :require="false"
+          :error="''"
+          :modelValue="form.commentsValue"
+          @update="updateComments"
         />
 
         <div class="app__form-btn-container">
-          <button class="app__form-btn" type="submit">call me</button>
-          <p class="app__form-agree-text">
+          <button
+            class="app__form-btn"
+            type="submit"
+            @click.prevent="handleSubmit"
+          >
+            call me
+          </button>
+          <p class="app__form-policy">
             By pressing “Send” button I agree with
-            <span class="app__from-policy">Privacy Policy</span>
+            <span class="app__form-policy_span">Privacy Policy</span>
           </p>
         </div>
       </form>
@@ -44,6 +76,60 @@
 import InputComponent from "./components/InputComponent.vue";
 export default {
   comments: { InputComponent },
+  data: function () {
+    return {
+      form: {
+        nameValue: "",
+        phoneValue: "+7",
+        commentsValue: "",
+        tempValue: 50,
+      },
+      errors: {
+        name: "",
+        phone: "",
+      },
+    };
+  },
+  methods: {
+    updateName(val) {
+      this.form.nameValue = val;
+      if (val.length < 2) {
+        this.errors.name = "Введите свое имя.";
+      } else {
+        this.errors.name = "";
+      }
+    },
+    updatePhone(val) {
+      const reg = /[^+\d]/g;
+      this.form.phoneValue = val;
+
+      if (val.length != 12 || reg.test(val)) {
+        this.errors.phone = "Номер телефона некорректен.";
+      } else {
+        this.errors.phone = "";
+      }
+    },
+    updateTemp(val) {
+      this.form.tempValue = val;
+    },
+    updateComments(val) {
+      this.form.commentsValue = val;
+    },
+    handleSubmit() {
+      if (this.isValidForm) {
+        console.log(JSON.parse(JSON.stringify(this.form)));
+      } else {
+        console.log("Заполните форму!");
+      }
+    },
+  },
+  computed: {
+    isValidForm() {
+      return this.form.nameValue.length < 2 || this.form.phoneValue.length != 12
+        ? false
+        : true;
+    },
+  },
 };
 </script>
 
@@ -97,6 +183,7 @@ export default {
   flex-direction: row;
   justify-content: flex-start;
   margin-top: 65px;
+  margin-bottom: 30px;
   width: 100%;
 }
 
@@ -121,6 +208,60 @@ export default {
   width: 192px;
   height: 47px;
   opacity: 0.2;
+}
+
+.app__form {
+  width: 538px;
+}
+
+.app__form-btn-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.app__form-btn {
+  border: 1px solid #eeebe6;
+  border-radius: 100px;
+  background-color: transparent;
+  color: #eeebe6;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.3;
+  text-align: center;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  padding: 20px 60px;
+  margin-right: 72px;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.app__form-btn:hover {
+  opacity: 0.7;
+  transition: all 0.3s ease-in-out;
+}
+
+.app__form-policy {
+  max-width: 205px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 1.5;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+}
+
+.app__form-policy_span {
+  color: #ff69b4;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.app__form-policy_span:hover {
+  color: #ff0080;
+  transition: all 0.3s ease-in-out;
 }
 
 .footer {
@@ -159,5 +300,124 @@ export default {
 .footer__policy:hover {
   opacity: 0.6;
   transition: all 0.3s ease-in-out;
+}
+
+@media screen and (max-width: 1255px) {
+  .app__title {
+    font-size: 46px;
+    line-height: 1.1;
+  }
+
+  .app__info {
+    margin-right: 30%;
+  }
+
+  .app__form {
+    width: 438px;
+  }
+
+  .app__form-btn {
+    padding: 15px 40px;
+  }
+
+  .footer__author {
+    margin-right: 100px;
+  }
+}
+
+@media screen and (max-width: 1042px) {
+  .app__form-policy {
+    max-width: 170px;
+    font-size: 12px;
+  }
+}
+
+@media screen and (max-width: 995px) {
+  #app {
+    min-width: 320px;
+    width: 100%;
+    padding: 20px 15px;
+  }
+
+  .app__content {
+    flex-direction: column;
+  }
+
+  .app__info {
+    flex-direction: row;
+    margin-right: 0;
+    margin-bottom: 60px;
+    justify-content: center;
+  }
+
+  .app__description {
+    margin-bottom: 0;
+    margin-right: 50px;
+    max-width: 300px;
+  }
+
+  .app__form {
+    width: 538px;
+    margin: 0 auto;
+  }
+
+  .footer {
+    bottom: 20px;
+    left: 15px;
+  }
+}
+
+@media screen and (max-width: 585px) {
+  .app__info {
+    justify-content: flex-start;
+  }
+
+  .app__description {
+    margin-right: 30px;
+    max-width: 200px;
+  }
+
+  .app__form {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .app__info {
+    align-items: center;
+  }
+
+  .app__logo {
+    width: 100px;
+    height: 26px;
+  }
+
+  .app__form-btn-container {
+    flex-direction: column;
+    align-items: start;
+  }
+
+  .app__form-btn {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
+
+  .app__form-policy {
+    max-width: 90%;
+  }
+
+  .footer__author {
+    margin-right: 40px;
+  }
+
+  .footer__author {
+    font-size: 11px;
+    margin-right: 20px;
+  }
+
+  .footer__policy {
+    font-size: 11px;
+  }
 }
 </style>
--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="input__container">
     <label class="input__label" :for="id">{{ label }}</label>
-    <input class="input" v-if="type != 'range'" :id="id" :type="type" />
-    <input class="input_range" v-else :id="id" type="range" />
-    <p class="input__hot" v-if="type == 'range'">HOT</p>
-    <p class="input__cold" v-if="type == 'range'">COLD</p>
+    <input
+      class="input"
+      :class="{ input_error: error != '' }"
+      v-if="type != 'range'"
+      :id="id"
+      :type="type"
+      :require="require"
+      :value="modelValue"
+      @input="updateInput"
+    />
+    <input
+      class="input__range"
+      :style="{ backgroundSize: modelValue + '% 100%' }"
+      v-else
+      :id="id"
+      type="range"
+      min="0"
+      max="100"
+      :value="modelValue"
+      @input="updateInput"
+    />
+    <p class="input__temp input__temp_hot" v-if="type === 'range'">HOT</p>
+    <p class="input__temp input__temp_cold" v-if="type === 'range'">COLD</p>
+    <p v-if="error != ''" class="input__error-text">
+      {{ error }}
+    </p>
   </div>
 </template>
 
@@ -15,9 +37,153 @@ export default {
     label: String,
     id: String,
     type: String,
+    require: Boolean,
+    modelValue: [String, Number],
+    error: String,
+  },
+  methods: {
+    updateInput(evt) {
+      this.$emit("update", evt.target.value);
+    },
   },
 };
 </script>
 
 <style>
+.input__container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 51px;
+  position: relative;
+}
+
+.input__label {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.5;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+}
+
+.input {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.5;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid rgba(238, 235, 230, 0.5);
+  color: #eeebe6;
+  padding: 3px 0;
+}
+
+.input_error {
+  border-bottom: 1px solid rgba(255, 0, 0, 0.5);
+  color: red;
+}
+
+.input__error-text {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.5;
+  color: red;
+  position: absolute;
+  top: 48px;
+  left: 0;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+.input:focus-visible {
+  outline: none;
+}
+
+.input__temp {
+  position: absolute;
+  top: 48px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 9px;
+  line-height: 150%;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+}
+
+.input__temp_hot {
+  left: 0;
+}
+
+.input__temp_cold {
+  right: 0;
+}
+
+.input__range {
+  appearance: none;
+  position: absolute;
+  top: 41px;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background: #5baaf9;
+  background-image: linear-gradient(#ff69b4, #ff69b4);
+  background-size: 50% 100%;
+  background-repeat: no-repeat;
+}
+
+.input__range::-webkit-slider-thumb {
+  appearance: none;
+  height: 13px;
+  width: 13px;
+  border-radius: 50%;
+  background: #b6a16b;
+  cursor: ew-resize;
+  transition: background 0.3s ease-in-out;
+}
+
+.input__range::-moz-range-thumb {
+  appearance: none;
+  height: 13px;
+  width: 13px;
+  border-radius: 50%;
+  background: #b6a16b;
+  cursor: ew-resize;
+  transition: background 0.3s ease-in-out;
+}
+
+.input__range::-ms-thumb {
+  appearance: none;
+  height: 13px;
+  width: 13px;
+  border-radius: 50%;
+  background: #b6a16b;
+  cursor: ew-resize;
+  transition: background 0.3s ease-in-out;
+}
+
+.input__range::-webkit-slider-runnable-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
+
+.input__range::-moz-range-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
+
+.input__range::-ms-track {
+  appearance: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
 </style>
